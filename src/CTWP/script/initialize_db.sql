b9999d13129cf800ebfc71668fcf9192292aeac3
brunoconterato <brunoconterato@gmail.com>
2024-11-25 19:22:17 -0300

    feat: add VSCode settings for PostgreSQL connection and update SQL script to reset database

--- src/CTWP/script/initialize_db.sql
+++ src/CTWP/script/initialize_db.sql
@@ -5,7 +5,7 @@ CREATE USER fiap_gs WITH PASSWORD 'fiap_gs';
 CREATE DATABASE gs_energia_residencial OWNER fiap_gs;
 
 -- Connect to the newly created database as the new user
-\c gs_energia_residencial fiap_gs
+-- \c gs_energia_residencial fiap_gs
 
 -- Grant all privileges on the database to the user (this is still needed for other operations)
 GRANT ALL PRIVILEGES ON DATABASE gs_energia_residencial TO fiap_gs;
@@ -25,3 +25,6 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO fiap_gs;
 
 -- Alternatively, grant specific privileges (more secure):
 --GRANT SELECT, INSERT, UPDATE, DELETE ON CONSUMO_RESIDENCIAL TO fiap_gs;
+
+-- Reset database
+TRUNCATE TABLE CONSUMO_RESIDENCIAL;
